a5c72212264a59244603fddc5d85dce98c45d943
Diogo-Fernandes <diogo.fernandes98@hotmail.com>
2021-01-19 04:25:42 +0000

    Day and Night Cycle

---
+++ Assets/MeterPlayer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeterPlayer : MonoBehaviour
+{
+    public float maxTime = 24;
+    public float currentTime;
+
+    public MeterScript meterScript;
+
+    private float timedebug;
+    private float fillTime =0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = 8;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timedebug = Mathf.Lerp(currentTime, maxTime, fillTime);
+        fillTime += 0.0055f * Time.deltaTime;
+
+
+        if (timedebug == 24)
+        {
+            Debug.Log("CHEGOU");
+        }
+        else
+        {
+            meterScript.SetTime(timedebug);
+        }
+        Debug.Log(timedebug);
+    }
+}
---
+++ Assets/MeterScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MeterScript : MonoBehaviour
+  
+{
+    public Slider slider;
+
+    public void SetMaxTime( float time)
+    {
+        slider.maxValue = time;
+        
+    }
+
+    public void SetTime(float time)
+    {
+        slider.value = time;
+    }
+}
